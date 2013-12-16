17fbce56ecda5efe0fa386a434ba08f36b618a7f
ankit1910 <ankit1910@gmail.com>
2013-12-17 01:23:12 +0530

    base structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vbet::Application.config.secret_key_base = '5a7a75ac72d51332e337effbf0ef80517bbba3f89553e38ab868b215f7301971d8b5f65ebcb72a7bbf068fd97b65201e3826fa34e27732279993881135bd78ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vbet::Application.config.session_store :cookie_store, key: '_vbet_session'
